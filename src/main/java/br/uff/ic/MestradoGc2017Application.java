dc46bc2cbe0de425e8ad60ad0e5d25173a437d8d
gsag <gsag.dh@gmail.com>
2017-04-13 11:15:25 -0300

    REFS #1: Adicionando projeto spring-boot inicial.

---
+++ src/main/java/br/uff/ic/MestradoGc2017Application.java
@@ -0,0 +1,12 @@
+package br.uff.ic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MestradoGc2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MestradoGc2017Application.class, args);
+	}
+}
---
+++ src/main/java/br/uff/ic/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.uff.ic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MestradoGc2017Application.class);
+	}
+
+}
