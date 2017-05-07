f41bddd84bc5307350b0e0276b4e6c004949cf1c
gsag <gsag.dh@gmail.com>
2017-05-07 17:39:00 -0300

    REFS #4: Montando layout inicial para começar a implementação.

---
+++ src/main/java/br/uff/ic/controller/IndexController.java
@@ -0,0 +1,25 @@
+package br.uff.ic.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by guilherme on 07/05/17.
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    private final Logger logger = Logger.getLogger(IndexController.class);
+
+    public IndexController() {
+    }
+
+    @GetMapping
+    public String getLayout(){
+        logger.info("Acessando controlador temporário do layout");
+        return "index";
+    }
+}
---
+++ src/main/resources/static/js/ckeditor_script.js
@@ -0,0 +1,7 @@
+/**
+ * Created by guilherme on 07/05/17.
+ */
+$(document).ready(function() {
+    "use strict";
+    var instance = CKEDITOR.replace('ckeditor');
+});
