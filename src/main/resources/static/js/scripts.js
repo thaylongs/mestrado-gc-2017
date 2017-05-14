9e02073f0e28f6d899cf6c1bad5fdebb33537006
gsag <gsag.dh@gmail.com>
2017-05-14 11:31:02 -0300

    REFS #6: Adicionando animações no botão de salvar.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -8,10 +8,12 @@ $(document).ready(function() {
     $.get('/last',function (data) {
         if(data) {
             updateViewContent(data);
+            applyAnimations();
         }
     });
 
     $('#btn-save-document').on('click', function () {
+        var that = $(this);
         var editorContent = CKEDITOR.instances['ckeditor'].getData();
         var documentName = $("#document_name").val();
         documentName = documentName ? documentName : 'Documento sem nome';
@@ -22,6 +24,8 @@ $(document).ready(function() {
             'date': date.toDateString() + " " + date.toTimeString()
         };
 
+        requestOperationState(that, 'fa-floppy-o', true);
+
         $.ajax({
             url: '/post',
             data: JSON.stringify(json),
@@ -30,8 +34,29 @@ $(document).ready(function() {
             type: "POST"
         }).done(function (data) {
             updateViewContent(JSON.parse(data));
+        }).always(function () {
+            requestOperationState(that, 'fa-floppy-o', false);
         });
     });
+
+    var requestOperationState = function (el, elIconClass, onSend) {
+        if(onSend){
+            el.addClass("disabled");
+            var icon = el.find("i");
+            icon.removeClass(elIconClass);
+            icon.addClass("fa-hourglass-o");
+            el.find("span").text(el.data("loading-text"));
+        }else{
+            setTimeout(function () {
+                var span = el.find("span");
+                span.text(span.data("text"));
+                var icon = el.find("i");
+                icon.removeClass("fa-hourglass-o");
+                icon.addClass(elIconClass);
+                el.removeClass("disabled");
+            },1000);
+        }
+    }
     
     var updateViewContent = function (data) {
         var date = new Date(data.dataModificacao);
@@ -40,5 +65,20 @@ $(document).ready(function() {
         $("#document_name").val(data.titulo);
         $("#document_name_label").addClass("active");
         $("#ckeditor").val($.base64.decode(data.conteudo));
-    }
+    };
+
+    var applyAnimations = function () {
+        $('#btn-save-document').animateCss('fadeInRight');
+        $('#document_message').animateCss('fadeIn');
+        $('.brand > i').animateCss('flipInY');
+    };
+
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
 });
